9d36bcaed3e125709a283486dba2e4616e5ec956
JoanLazo <joan162581@gmail.com>
2019-01-15 13:03:59 -0500

    Se modifico .js

--- fetch-api-post/fetch-formulario.js
+++ fetch-api-post/fetch-formulario.js
@@ -2,8 +2,18 @@ const formulario = document.getElementById('formulario');
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('me diste un click')
+
     let datos = new FormData(formulario);
     console.log(datos);
     console.log(datos.get('usuario'));
     console.log(datos.get('pass'));
+
+    fetch('post.php',{
+        method: 'POST',
+        body: datos
+    })
+    .then( res => res.json())
+    .then(data => {
+   console.log(data)
+    })
 })  
