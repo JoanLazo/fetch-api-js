d98741bb1181008249a6020a877f9a3ea9108a35
JoanLazo <joan162581@gmail.com>
2019-01-25 18:55:47 -0500

    se agrego un div

--- practica-fetch/fecht-api.js
+++ practica-fetch/fecht-api.js
@@ -13,37 +13,37 @@ const contenido = document.querySelector('#contenido');
 
 // Consumiendo informacion de un link api
 
-// function traer(){
-//     fetch('https://randomuser.me/api/')
-//     .then( res => res.json())
-//     .then( data => {
-//         console.log(data.results['0'])
-//         contenido.innerHTML = `
-//         <img src="${data.results['0'].picture.large}" width="100px" class="img-fluid rounded-circle">
-//         <p>Nombre: ${data.results['0'].name.first}</p>`
-//     }) 
-// }
-    
-// Creando una tabla con Json
 function traer(){
-    fetch('tabla.json')
-    .then( res => res.json() )
+    fetch('https://randomuser.me/api/')
+    .then( res => res.json())
     .then( data => {
-        tabla(data)
+        console.log(data.results['0'])
+        contenido.innerHTML = `
+        <img src="${data.results['0'].picture.large}" width="100px" class="img-fluid rounded-circle">
+        <p>Nombre: ${data.results['0'].name.first}</p>`
     }) 
 }
-function tabla(data){
-    // console.log(data)
-    // contenido.innerHTML = ''
-    for(let valor of data){
-        // console.log(i.nombre)
-        contenido.innerHTML += `
-        <tr>
-        <th scope="row">${valor.id}</th>
-        <td>${valor.nombre}</td>
-        <td>${valor.email}</td>
-        <td>${valor.estado ? 'Activo': 'Eliminado'}</td>
-      </tr>`
+    
+// // Creando una tabla con Json
+// function traer(){
+//     fetch('tabla.json')
+//     .then( res => res.json() )
+//     .then( data => {
+//         tabla(data)
+//     }) 
+// }
+// function tabla(data){
+//     // console.log(data)
+//     // contenido.innerHTML = ''
+//     for(let valor of data){
+//         // console.log(i.nombre)
+//         contenido.innerHTML += `
+//         <tr>
+//         <th scope="row">${valor.id}</th>
+//         <td>${valor.nombre}</td>
+//         <td>${valor.email}</td>
+//         <td>${valor.estado ? 'Activo': 'Eliminado'}</td>
+//       </tr>`
 
-    }
-}
+//     }
+// }
